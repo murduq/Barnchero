f654f12fec1bd33b3089aa683f9b9dac1b809901
Kian Murdock <kianmurdock07@gmail.com>
2021-08-31 12:33:09 -0400

    item qol updates
    added a magnet effect to attract items to player, flash item when it's about to despawn

--- Assets/Scripts/ItemController.cs
+++ Assets/Scripts/ItemController.cs
@@ -6,9 +6,42 @@ public class ItemController : MonoBehaviour
 {
     public float lifetime = 5.0f;
     public float timer;
+    public GameObject[] players;
+    public float speed = 10f;
     void Awake()
     {
         timer = lifetime;
         Destroy(this.gameObject, lifetime);
+        players = GameObject.FindGameObjectsWithTag("Player");
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer <=1.5f){
+            StartCoroutine(iFrameFlash());
+        }
+        if (Vector2.Distance(players[0].transform.position, transform.position) <= 1.75f){
+            Magnet();
+        }        
+    }
+
+    void Magnet()
+    {
+        Vector2 target = players[0].transform.position;
+        float moveSpeed = speed * Time.deltaTime;
+        transform.position =
+            Vector2.MoveTowards(transform.position, target, moveSpeed);
+    }
+
+    IEnumerator iFrameFlash()
+    {
+        bool flash = false;
+        while (true)
+        {
+            this.GetComponent<Renderer>().enabled = flash;
+            flash = !flash;
+            yield return new WaitForSeconds(.25f);
+        }
     }
 }
